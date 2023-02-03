cc49f4279521f61b19595bff0bff77db5a72bf88
mx <max@maxvl.com>
2023-02-02 23:50:36 -0500

    Basic token struct and constants

---
+++ token/token.go
@@ -0,0 +1,32 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF 	= "EOF"
+	// identifiers + literals
+	IDENT 	= "IDENT"
+	INT 	= "INT"
+	
+	// operators
+	ASSIGN 	= "="
+	PLUS 	= "+"
+	
+	// delimiters
+	COMMA 		= ","
+	SEMICOLON	= ";" 
+	LPAREN 		= "("
+	RPAREN 		= ")"
+	LBRACE		= "{"
+	RBRACE 		= "}"
+	
+	// keywords
+	FUNCTION 	= "FUNCTION"
+	LET 		= "LET"
+)
+
+type TokenType string
+
+type Token struct {
+	Type	TokenType
+	Literal string
+}
