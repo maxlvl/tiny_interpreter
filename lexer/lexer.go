c596d2e5269f4dacca1cb7921a4cd7d0cd0a6238
mx <max@maxvl.com>
2023-02-03 23:56:48 -0500

    fix up isletter and readIdent to lexer package

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -47,9 +47,30 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Type = token.EOF
 		tok.Literal = ""
+  default:
+    if isLetter(l.ch) {
+      tok.Literal = l.readIdentifier()
+      // TODO: FIX ME
+      tok.Type = token.LookupIndent(tok.Literal)
+      return tok
+    } else {
+      token = newToken(token.ILLEGAL, l.ch)
+    }
 	}
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+  position := l.position
+  for isLetter(l.ch) {
+    l.readChar()
+  }
+  return l.input[position: l.position]
+}
+
+func isLetter(ch byte) bool {
+  return 'a' <= ch && ch >= 'z' || 'A' <= ch && ch >= 'Z' || ch == '_'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
