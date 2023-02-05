7729fe7336e55b9dc3cbca07736b8f4a08fe7d9c
mx <max@maxvl.com>
2023-02-05 10:52:33 -0500

    eat whitespace and don't return token for it

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -30,6 +30,20 @@ func (l *Lexer) NextToken() token.Token {
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
+  case '+':
+    tok = newToken(token.PLUS, l.ch)
+  case '-':
+    tok = newToken(token.MINUS, l.ch)
+  case '!':
+    tok = newToken(token.BANG, l.ch)
+  case '/':
+    tok = newToken(token.SLASH, l.ch)
+  case '*':
+    tok = newToken(token.ASTERISK, l.ch)
+  case '<':
+    tok = newToken(token.LT, l.ch)
+  case '>':
+    tok = newToken(token.GT, l.ch)
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -38,8 +52,6 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.RPAREN, l.ch)
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
@@ -52,13 +64,30 @@ func (l *Lexer) NextToken() token.Token {
       tok.Literal = l.readIdentifier()
       tok.Type = token.LookupIndent(tok.Literal)
       return tok
-    } else {
+    } else if isDigit(l.ch) {
+      tok.Type = token.INT
+      tok.Literal = l.readNumber()
+      return tok
+    }else {
       tok = newToken(token.ILLEGAL, l.ch)
     }
 	}
 	l.readChar()
 	return tok
 }
+
+
+func (l *Lexer) readNumber() string {
+  position := l.position
+  for isDigit(l.ch) {
+    l.readChar()
+  }
+  return l.input[position: l.position]
+}
+
+func isDigit(ch byte) bool {
+  return '0' <= ch && ch <= '9'
+
 
 func (l *Lexer) readIdentifier() string {
   position := l.position
