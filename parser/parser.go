992135fd36a3083ea608692ac6962d8c639d8fd0
mx <max@maxvl.com>
2023-02-22 22:51:31 -0500

    Implement skeleton of parser

---
+++ parser/parser.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+  "treewalker/ast"
+  "treewalker/lexer"
+  "treewalker/token"
+)
+
+type Parser struct {
+  l *lexer.Lexer
+  curToken token.Token
+  peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+  p := &Parser{l: l}
+  // read two tokens, so curtoken and peektoken are both set
+  p.nextToken()
+  p.nextToken()
+  return p
+}
+
+func (p *Parser) nextToken() {
+  p.curToken = p.peekToken
+  p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+  return nil
+}
